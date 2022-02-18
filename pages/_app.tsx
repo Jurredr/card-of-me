479d8f1cf861322b0fc6819cb3dbe5294320c1f8
Jurredr <github@jurre.me>
2022-02-18 19:02:01 +0100

    Added Montserrat and icon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,23 @@
-import { NextUIProvider } from '@nextui-org/react'
+import { createTheme, NextUIProvider } from '@nextui-org/react'
 import type { AppProps } from 'next/app'
+import '@fontsource/montserrat'
 import 'windi.css'
+import '../styles/globals.scss'
+
+const theme = createTheme({
+  type: 'light',
+  theme: {
+    colors: {},
+    space: {},
+    fonts: {
+      montserrat: 'Montserrat'
+    }
+  }
+})
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <NextUIProvider>
+    <NextUIProvider theme={theme}>
       <Component {...pageProps} />
     </NextUIProvider>
   )
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,17 @@
-import { Button } from '@nextui-org/react'
 import type { NextPage } from 'next'
+import Image from 'next/image'
 
 const Home: NextPage = () => {
   return (
-    <div className="">
-      <Button>Default</Button>
-      <Button disabled>Default</Button>
+    <div className="flex">
+      <Image
+        className="nextimg"
+        src="/icon.svg"
+        alt=""
+        width={30}
+        height={30}
+      />
+      <p className=" text-red-400">CardOf.Me</p>
     </div>
   )
 }
