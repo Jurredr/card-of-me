15a4cc15425bf5654a742024fef48a35c867dfc8
Jurredr <github@jurre.me>
2022-02-18 17:24:57 +0100

    Changed tab size to 4

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
-import type { NextPage } from 'next';
+import type { NextPage } from 'next'
 
 const Home: NextPage = () => {
-    return <div></div>;
-};
+    return <div></div>
+}
 
-export default Home;
+export default Home
