591b5e2159835c61c0a3007b7979f03bc4bfe19e
Jurredr <github@jurre.me>
2022-02-18 17:42:51 +0100

    Added WindiCSS and NextUI

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,13 @@
+import { NextUIProvider } from '@nextui-org/react'
 import type { AppProps } from 'next/app'
+import 'windi.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />
+  return (
+    <NextUIProvider>
+      <Component {...pageProps} />
+    </NextUIProvider>
+  )
 }
 
 export default MyApp
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,13 @@
+import { Button } from '@nextui-org/react'
 import type { NextPage } from 'next'
 
 const Home: NextPage = () => {
-    return <div></div>
+  return (
+    <div className="">
+      <Button>Default</Button>
+      <Button disabled>Default</Button>
+    </div>
+  )
 }
 
 export default Home
