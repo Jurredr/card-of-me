4457b288b007023c9b74b1e5e3f084eee04b8fec
Jurredr <github@jurre.me>
2022-02-19 13:28:09 +0100

    Sign in and sign up page base layouts

--- pages/sign-up.tsx
+++ pages/sign-up.tsx
@@ -1,7 +1,34 @@
+import { Button } from '@nextui-org/react'
 import { NextPage } from 'next'
+import Link from 'next/link'
+import { FiChevronLeft } from 'react-icons/fi'
 
 const SignUp: NextPage = () => {
-  return <div>sign-up</div>
+  return (
+    <div className="w-full h-full grid grid-cols-2 items-center justify-center">
+      {/* Left */}
+      <div className="bg-black z-10 h-screen w-screen lg:w-[50vw]">
+        {/* Back button */}
+        <Link href="/" passHref>
+          <Button
+            className="absolute top-5 left-5 py-2 px-4 rounded-full bg-dark-300"
+            auto
+          >
+            <div className="flex justify-center items-center gap-1">
+              <FiChevronLeft size={20} />
+              Back Home
+            </div>
+          </Button>
+        </Link>
+
+        {/* Login */}
+        <div></div>
+      </div>
+
+      {/* Right */}
+      <div className="bg-gradient-to-br from-cyan-500 to-purple-600 relative hidden lg:flex self-center items-center float-right w-[50vw] h-screen"></div>
+    </div>
+  )
 }
 
 export default SignUp
