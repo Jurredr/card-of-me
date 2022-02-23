38ddf4467ddebcefbce2cbc8c33949e198335e54
Jurredr <github@jurre.me>
2022-02-23 22:28:22 +0100

    Added function to get user by username

--- src/carduser.ts
+++ src/carduser.ts
@@ -1,9 +1,18 @@
 import { getDocs, limit, query, where } from 'firebase/firestore'
 import { Users } from './firebase'
 
-// @ts-expect-error
-export async function getUserByEmail(email) {
+export async function getUserByEmail(email: string) {
   const userQuery = query(Users, where('email', '==', email), limit(1))
+  const userDocs = await getDocs(userQuery)
+  userDocs.forEach((doc) => {
+    console.log(doc.id, ' => ', doc.data())
+  })
+  if (userDocs.empty) return null
+  return userDocs.docs[0]
+}
+
+export async function getUserByUsername(username: string) {
+  const userQuery = query(Users, where('username', '==', username), limit(1))
   const userDocs = await getDocs(userQuery)
   userDocs.forEach((doc) => {
     console.log(doc.id, ' => ', doc.data())
