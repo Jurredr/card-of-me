53f7a0acb9d715e065d9f49ea2cdd5e3a7341124
Jurredr <github@jurre.me>
2022-03-01 18:05:40 +0100

    Added expires field to Session type

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -7,6 +7,7 @@ declare module 'next-auth' {
    */
   interface Session {
     user: User
+    expires: string
   }
 
   /**
@@ -22,7 +23,6 @@ declare module 'next-auth' {
       lastName: string
     }
     image: string | null | undefined
-    card: []
   }
 
   /**
